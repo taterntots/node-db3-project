d29dda88d1baeafe24a7835ea342fa82e559fa35
taterntots <matthew.a.weidner@gmail.com>
2020-01-22 16:35:37 -0500

    solved the last two queries

--- queries.sql
+++ queries.sql
@@ -16,6 +16,18 @@ WHERE o.OrderDate < '2012-08-09'
 ORDER BY s.CompanyName;
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+SELECT p.ProductName, od.Quantity
+FROM [OrderDetail] as od
+JOIN Product as p
+  ON od.ProductId = p.Id
+  AND od.OrderId = 10251
+ORDER BY p.ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT o.Id, c.CompanyName, e.LastName
+FROM [Order] as o
+JOIN Customer as c
+  ON c.Id = o.CustomerId
+JOIN Employee as e
+  ON o.EmployeeId = e.Id
+ORDER BY e.LastName;
